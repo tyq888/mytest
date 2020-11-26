b4da20a5d00246443fa74f731e804d84de82e692
daitt <yuluo1736408906@163.com>
2020-11-26 14:05:30 +0800

    啊这

---
+++ HelloProjet/src/aaaoooT.java
@@ -0,0 +1,5 @@
+public class aaaoooT {
+    public static void main(String[] args) {
+        System.out.println("美女");
+    }
+}
--- HelloProjet/src/testDemo.java
+++ HelloProjet/src/testDemo.java
@@ -2,5 +2,6 @@ public class testDemo {
     public static void main(String[] args) {
         int a = 1;
         //的故事的发生地
+        //东东东东
     }
 }
